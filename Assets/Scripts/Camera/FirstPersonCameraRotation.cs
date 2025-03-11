a44d6a4d06152163c71d3e6fde73c66eefc15681
mmorri22 <mmorri22@terpmail.umd.edu>
2025-03-10 22:07:26 -0400

    babies first camera

---
+++ Assets/Scripts/Camera/FirstPersonCameraRotation.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour {
+
+	public float Sensitivity {
+		get { return sensitivity; }
+		set { sensitivity = value; }
+	}
+	[Range(0.1f, 9f)][SerializeField] float sensitivity = 2f;
+	[Range(0f, 90f)][SerializeField] float yRotationLimit = 88f;
+
+	Vector2 rotation = Vector2.zero;
+	const string xAxis = "Mouse X";
+	const string yAxis = "Mouse Y";
+
+	void Start()
+	{
+    	Cursor.lockState = CursorLockMode.Locked;
+    	Cursor.visible = false;
+
+	}
+	void LateUpdate(){
+
+		rotation.x += Input.GetAxis(xAxis) * sensitivity;
+		rotation.y += Input.GetAxis(yAxis) * sensitivity;
+		rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+
+		var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+		var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+
+		transform.localRotation = xQuat * yQuat; 
+
+	}
+}
